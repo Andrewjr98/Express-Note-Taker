ffe5033cd11f63dd83879b39b97a4d7d2ed140c8
Andrew <andrewrandolph98@gmail.com>
2022-12-18 22:12:06 -0600

    Removed Develop folder and deleted it

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,6 +4,13 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+let getStartedBtn;
+if (window.location.pathname === '/') {
+  console.log("ahhhhhhhhh");
+getStartedBtn = document.querySelector('.getStarted');
+
+}
+
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
@@ -173,11 +180,16 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
+const switchPage = ()=>{
+  window.location.href = window.location.hostname+"/notes";
+}
+
 if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
+  getStartedBtn.addEventListener("click",switchPage )
 }
 
 getAndRenderNotes();
